e25165e4e0087998a2180595c03ea6c218b864d4
mohamedkhamees683-cmyk <mohamedkhamees683@gmail.com>
2025-09-13 18:02:49 +0300

    case 1

--- the game.py
+++ the game.py
@@ -8,3 +8,9 @@ print("3. print a random word")
 print("4. Multiply two numbers")
 
 enter=input("enter your choice :")
+
+match enter :
+    case "1":
+        num1=int(input("Enter first number:"))
+        num2=int(input("Enter second number:"))
+        print(f"the sum of {num1} and {num2} is{num1+num2}")
