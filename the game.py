78a675f32d99804f5ae2378fbd36968b7ec6205e
mohamedkhamees683-cmyk <mohamedkhamees683@gmail.com>
2025-09-13 17:56:59 +0300

    Create the game.py

---
+++ the game.py
@@ -0,0 +1,10 @@
+import random
+
+print("Welcome to this python Application")
+print("What do you want this application to do?")
+print("1. Add two numbers")
+print("2. print a random number")
+print("3. print a random word")
+print("4. Multiply two numbers")
+
+enter=input("enter your choice :")
